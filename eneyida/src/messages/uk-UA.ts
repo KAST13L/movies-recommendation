6479516be96e1709fa4fb407d3756bb2f1375fa7
KAST13L <o4enbsmeshno@gmail.com>
2023-04-03 19:37:40 +0300

    fix: change english locale state

--- eneyida/src/messages/uk-UA.ts
+++ eneyida/src/messages/uk-UA.ts
@@ -2,21 +2,21 @@ import {LOCALES} from "../variables";
 
 export default {
     [LOCALES.UKRAINIAN]: {
-        'navigation': {
-            'home': 'Рекомендації фільмів',
-            'settings': 'Налаштування',
-            'en':'АНГ',
-            'uk':'УКР'
+        navigation: {
+            home: 'Рекомендації фільмів',
+            settings: 'Налаштування',
+            en:'АНГ',
+            uk:'УКР'
         },
-        'no_selected_movies': 'Немає вибраних фільмів...',
-        'form_input_placeholder': 'введіть назву для сформованого спику фільмів',
-        'share_with_friends': 'Поділитися з друзями',
-        'copied': 'Скопійовано!',
-        'select': 'Вибрати',
-        'required': 'Обовязкове поле',
-        'title_movies_list':'Назва списку фільмів:',
-        'share_link':'Поділитися посиланям через:',
-        'copy_link':'Або скопіювати посилання:',
+        no_selected_movies: 'Немає вибраних фільмів...',
+        form_input_placeholder: 'введіть назву для сформованого спику фільмів',
+        share_with_friends: 'Поділитися з друзями',
+        copied: 'Скопійовано!',
+        select: 'Вибрати',
+        required: 'Обовязкове поле',
+        title_movies_list:'Назва списку фільмів:',
+        share_link:'Поділитися посиланям через:',
+        copy_link:'Або скопіювати посилання:',
         filters: {
             sort_by: 'Сортувати по',
             sort_direction: 'Напрямок',
@@ -26,13 +26,13 @@ export default {
             genre: 'Жанр',
             submit: 'Пошук',
             sort: {
-                'popularity': 'Популярність',
-                'release_date': 'Дата випуску',
-                'revenue': 'Дохід',
-                'primary_release_date': 'Першочергова дата релізу',
-                'original_title': 'Оригінальна назва',
-                'vote_average': 'Середня оцінка',
-                'vote_count': 'Кількість оцінок'
+                popularity: 'Популярність',
+                release_date: 'Дата випуску',
+                revenue: 'Дохід',
+                primary_release_date: 'Першочергова дата релізу',
+                original_title: 'Оригінальна назва',
+                vote_average: 'Середня оцінка',
+                vote_count: 'Кількість оцінок'
             },
             sort_direction_options: {
                 asc: 'ASC',
