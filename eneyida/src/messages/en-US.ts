daf6e2f6f6fd7c4ea45519ba164658ca088a21f7
KAST13L <o4enbsmeshno@gmail.com>
2023-04-03 19:39:33 +0300

    fix: change english locale state

--- eneyida/src/messages/en-US.ts
+++ eneyida/src/messages/en-US.ts
@@ -2,21 +2,21 @@ import {LOCALES} from "../variables";
 
 export default {
     [LOCALES.ENGLISH]: {
-        'navigation': {
-            'home': 'Movies recommendation',
-            'settings': 'Settings',
-            'en':'en',
-            'uk':'uk'
+        navigation:{
+            home: 'Movies recommendation',
+            settings: 'Settings',
+            en:'en',
+            uk:'uk'
         },
-        'no_selected_movies': 'No selected movies...',
-        'form_input_placeholder': 'enter a title for the movie list',
-        'share_with_friends': 'Share with friends',
-        'copied': 'Copied!',
-        'select': 'Add',
-        'required': 'required field',
-        'title_movies_list':'Title of the movies list:',
-        'share_link':'Share this link by:',
-        'copy_link':'Or copy link:',
+        no_selected_movies: 'No selected movies...',
+        form_input_placeholder: 'enter a title for the movie list',
+        share_with_friends: 'Share with friends',
+        copied: 'Copied!',
+        select: 'Add',
+        required: 'required field',
+        title_movies_list:'Title of the movies list:',
+        share_link:'Share this link by:',
+        copy_link:'Or copy link:',
         filters: {
             sort_by: 'Sort by',
             sort_direction: 'Sort direction',
@@ -26,19 +26,18 @@ export default {
             genre: 'Genre',
             submit: 'Submit',
             sort: {
-                'popularity': 'Popularity',
-                'release_date': 'Release date',
-                'revenue': 'Revenue',
-                'primary_release_date': 'Primary release date',
-                'original_title': 'Original title',
-                'vote_average': 'Vote average',
-                'vote_count': 'Vote count'
+                popularity: 'Popularity',
+                release_date: 'Release date',
+                revenue: 'Revenue',
+                primary_release_date: 'Primary release date',
+                original_title: 'Original title',
+                vote_average: 'Vote average',
+                vote_count: 'Vote count'
             },
             sort_direction_options: {
                 asc: 'ASC',
                 desc: 'DESC'
             }
         }
-
     }
 };
